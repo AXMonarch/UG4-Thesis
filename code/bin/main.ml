d0ff8b8bb65c0da118e971daa6b74f23b528eb68
AXMonarch <arindampriv@gmail.com>
2026-01-22 10:08:08 +0000

    benchmarks

--- code/bin/main.ml
+++ code/bin/main.ml
@@ -7,14 +7,12 @@ let () =
   
   (* Particle Filter demos *)
   print_endline "--- Particle Filter Variants ---";
-  Pf.Pf_base.demo_toy_hmm ();
   Pf.Multinomial_pf.demo_multinomial_pf ();
   Pf.Resample_move_pf.demo_resample_move_pf ();
   
   (* Metropolis-Hastings demos *)
   print_endline "\n--- Metropolis-Hastings Variants ---";
   Mh.Mh_base.demo_toy_hmm ();
-  Mh.Independence_mh.demo_independence_mh ();
   Mh.Single_site_mh.demo_single_site_mh ();
   Mh.Particle_mh.demo_particle_mh ();
   
